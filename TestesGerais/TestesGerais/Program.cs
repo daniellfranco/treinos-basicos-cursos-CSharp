0093f1320913c389d686ae66ff17607b2e4b7b76
daniellfranco <daniel.luis.franco@gmail.com>
2022-10-13 17:58:59 -0300

    feito alguns testes em codigos do curso

--- TestesGerais/TestesGerais/Program.cs
+++ TestesGerais/TestesGerais/Program.cs
@@ -1,12 +1,16 @@
-﻿
+﻿using TestesGerais;
 
 
-//teste listas
+
+
+/*
+
+// Teste listas
 
 List<string> listaString = new List<string>();
 List<float> listaFloat = new List<float>();
 
-//add um item por vez, não da para por virgula e adicionar varios.
+// Add um item por vez, não da para por virgula e adicionar varios.
 listaString.Add("aaaa");
 
 listaFloat.Add(2.58f);
@@ -14,3 +18,14 @@ listaFloat.Add(2.58f);
 Console.WriteLine(listaString[0]);
 Console.WriteLine(listaFloat[0]);
 Console.WriteLine(listaFloat.Count);
+
+
+// Teste summary, passe o mouse sobre o nome da classe e do metodo
+TesteSummary testeSummary = new TesteSummary();
+
+testeSummary.PasseOMouseAquiSummary();
+
+// Quando você está digitando os parametros ele mostra o comentario exclusivo do parametro para documentar sua função. 
+Console.WriteLine(testeSummary.SomaPasseOMouseSummary(2, 3));
+
+*/
---
+++ TestesGerais/TestesGerais/TesteSummary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestesGerais
+{
+    /// <summary>
+    /// Teste desta tag summary, ela mostra informação da classe quando passa o mouse em cima.
+    /// </summary>
+    internal class TesteSummary
+    {
+        /// <summary>
+        /// Teste tag summary, ela também mostra informação do metodo.
+        /// </summary>
+        public void PasseOMouseAquiSummary()
+        {
+            Console.WriteLine("Passe o mouse em cima do metodo para ver o texto");
+        }
+
+        /// <summary>
+        /// Soma dois valores
+        /// </summary>
+        /// <param name="x">Primeiro valor a ser somado</param>
+        /// <param name="y">Segundo valor a ser somado</param>
+        /// <returns>Retorna a soma dos valores</returns>
+        public int SomaPasseOMouseSummary(int x, int y)
+        {
+            //Console.WriteLine($"Resultado soma {x} + {y} = {x + y}");
+            return x + y;
+        }
+    }
+}
