b61e3261acf5cd186335b57fb724de20df2b8c9b
daniellfranco <daniel.luis.franco@gmail.com>
2022-10-13 11:57:48 -0300

    criado projeto TestesGerais para executar alguns testes simples em codigos

---
+++ TestesGerais/TestesGerais/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+//teste listas
+
+List<string> listaString = new List<string>();
+List<float> listaFloat = new List<float>();
+
+//add um item por vez, não da para por virgula e adicionar varios.
+listaString.Add("aaaa");
+
+listaFloat.Add(2.58f);
+
+Console.WriteLine(listaString[0]);
+Console.WriteLine(listaFloat[0]);
+Console.WriteLine(listaFloat.Count);
