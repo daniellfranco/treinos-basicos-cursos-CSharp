b44815bb86fe6473dacd7e61208c811797b5a954
daniellfranco <daniel.luis.franco@gmail.com>
2022-10-17 17:34:19 -0300

    finalizado ExemploExplorando

--- Novos/ExemploExplorando/Models/Pessoa.cs
+++ Novos/ExemploExplorando/Models/Pessoa.cs
@@ -7,15 +7,27 @@ namespace ExemploExplorando.Models
 {
     public class Pessoa
     {
-        private string _nome;
+        //construtor basico
+        public Pessoa() { }
 
+        //construtor que ja adiciona os dados
+        public Pessoa(string nome, string sobrenome)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+        }
+
+
+
+        private string _nome; //private só pode ser acessado pela classe.
+        private int _idade;
 
         public string Nome
         {
-            get //tratando o get
-            {
-                return _nome.ToUpper(); //retorna o nome em maiusculo
-            }
+            //tratando o get
+            //quando o tratamento é muito simples, o ideal é usar essa forma para simplificar.
+            get => _nome.ToUpper(); //retorna o nome em maiusculo
+
 
             set //tratando o set
             {
@@ -28,11 +40,28 @@ namespace ExemploExplorando.Models
                 _nome = value;
             }
         }
-        public int Idade { get; set; }
+
+        public string Sobrenome { get; set; }
+
+        //essa propriedade é somente get, e ela automaticamente recebe nome e sobrenome concatenado.
+        public string NomeCompleto => $"{Nome} {Sobrenome}".ToUpper();
+
+        public int Idade
+        {
+            get => _idade;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("A idade não pode ser menor do que zero");
+                }
+                _idade = value;
+            }
+        }
 
         public void Apresentar()
         {
-            Console.WriteLine($"Nome: {Nome}, Idade: {Idade}");
+            Console.WriteLine($"Nome: {NomeCompleto}, Idade: {Idade}");
         }
 
 
--- Novos/ExemploExplorando/Program.cs
+++ Novos/ExemploExplorando/Program.cs
@@ -2,5 +2,35 @@
 
 Pessoa p1 = new Pessoa();
 p1.Nome = "Daniel";
+p1.Sobrenome = "Franco";
+
+Pessoa p2 = new Pessoa("Nayala", "Forest");
+
+/* esse nome: e sobrenome:, é o nome do parametro, serve apenas de 
+recurso visual para melhorar a legibilidade, e tem que ser o mesmo
+ nome do parametro senao nao funciona. */
+Pessoa p3 = new Pessoa(nome: "Ashley", sobrenome: "Evergarden");
+
+/* na verdade também serve pra caso queira inverter a ordem do
+ parametro, mas porque você vai querer fazer isso nao sei. */
+Pessoa p4 = new Pessoa(sobrenome: "Evergarden", nome: "Ashley");
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.AdicionarAluno(p3);
+cursoDeIngles.AdicionarAluno(p4);
+cursoDeIngles.ListarAlunos();
+
+
+/*
+Pessoa p1 = new Pessoa();
+p1.Nome = "Daniel";
+p1.Sobrenome = "Franco";
 p1.Idade = 27;
 p1.Apresentar();
+
+*/
