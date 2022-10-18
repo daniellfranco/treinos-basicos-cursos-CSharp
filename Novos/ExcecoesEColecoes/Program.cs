e73840e2a50d023de32ea799442432ddaba570ac
daniellfranco <daniel.luis.franco@gmail.com>
2022-10-18 18:07:18 -0300

    criado novo projeto

---
+++ Novos/ExcecoesEColecoes/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+try
+{
+    string[] linhas = File.ReadAllLines("Arquivos/arquivo-Leitura.txt");
+
+    foreach (string linha in linhas)
+    {
+        Console.WriteLine(linha);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ocorreu uma exceção genérica. {ex.Message}");
+}
