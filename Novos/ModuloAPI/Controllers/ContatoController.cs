aa6b6d85be88ac781830f2dc20625ed2107e1e4f
daniellfranco <daniel.luis.franco@gmail.com>
2022-11-03 17:10:50 -0300

    adicionado controller para cantatos

---
+++ Novos/ModuloAPI/Controllers/ContatoController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ModuloAPI.Context;
+using ModuloAPI.Entities;
+
+namespace ModuloAPI.Controllers
+{
+    //basico para um controller é esses dois
+    [ApiController]
+    [Route("[controller]")]
+    //e herdar desse ControllerBase
+    public class ContatoController : ControllerBase
+    {
+        private readonly AgendaContext _context;
+
+        public ContatoController(AgendaContext context)
+        {
+            _context = context;
+        }
+
+        //Com isso identifica que é um metodo POST
+        //dá para botar um nome de caminho, mas no post diz que não precisa.
+        [HttpPost]
+        public IActionResult Create(Contato contato)
+        {
+            _context.Add(contato);
+            _context.SaveChanges();
+            return Ok(contato);
+        }
+    }
+}
