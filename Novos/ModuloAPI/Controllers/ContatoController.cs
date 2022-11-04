9e95928bbabdd69e861a198cf0da7e8b3102c55a
daniellfranco <daniel.luis.franco@gmail.com>
2022-11-04 17:45:21 -0300

    atualizado ContatoController

--- Novos/ModuloAPI/Controllers/ContatoController.cs
+++ Novos/ModuloAPI/Controllers/ContatoController.cs
@@ -30,5 +30,35 @@ namespace ModuloAPI.Controllers
             _context.SaveChanges();
             return Ok(contato);
         }
+
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+
+            if (contato == null)
+                return NotFound();
+
+            return Ok(contato);
+
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Contato contato)
+        {
+            var contatoBanco = _context.Contatos.Find(id);
+
+            if (contato == null)
+                return NotFound();
+
+            contatoBanco.Nome = contato.Nome;
+            contatoBanco.Telefone = contato.Telefone;
+            contatoBanco.Ativo = contato.Ativo;
+
+            _context.Contatos.Update(contatoBanco);
+            _context.SaveChanges();
+
+            return Ok(contatoBanco);
+        }
     }
 }
